6434f8e435671a055b3cfe321cbf03544c32b318
ghiegie <giancarlogarcia.gallano@gmail.com>
2023-12-22 16:56:56 +0800

    2023-12-22

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env, io, fs
+};
+
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        println!("usage: rlox [script]");
+        return Ok(());
+    } else if args.len() == 1 {
+        runfile(&args[0]);
+    } else {
+        runPrompt()
+    }
+
+    Ok(())
 }
+
+fn runfile(path: &String) -> Result<(), io::Error> {
+    let bytes = fs::
+    Ok(())
+}
+
+fn runPrompt() {}
